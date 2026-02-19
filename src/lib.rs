8bf8f1b4c2132c16d2f0649e4d6b53be92ba6d56
hatakelab <htkr1126@icloud.com>
2026-02-19 11:58:13 +0900

    修正

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
 mod wnd;
 
 use wasm_bindgen::prelude::*;
-use web_sys::{
-    HtmlInputElement,
-};
-use wnd::*;
 
 #[wasm_bindgen]
 pub fn run(wnd_id: &str, msg_id: &str) {
-    let wnd = Wnd::new(wnd_id, msg_id);
+    let wnd = wnd::Wnd::new(wnd_id, msg_id);
     wnd.draw();
 }
--- src/wnd.rs
+++ src/wnd.rs
@@ -1,14 +1,11 @@
 use wasm_bindgen::prelude::*;
-use web_sys::{
-    HtmlInputElement,
-};
 
 const WINDOW: web_sys::Window = web_sys::window().expect("グローバルな「Window」がありません");
 const DOCUMENT: web_sys::Document = WINDOW.document().expect("「Window」に「Document」がありません");
 
 pub enum WndMode {
     Home,
-};
+}
 
 #[derive(Default)]
 pub struct Wnd {
@@ -17,7 +14,7 @@ pub struct Wnd {
     msg_id: String,
 }
 
-impl struct Wnd {
+impl Wnd {
     pub fn new(id: &str, msg_id: &str) -> Self {
         let wnd = Self::default();
         wnd.id = id;
@@ -27,8 +24,8 @@ impl struct Wnd {
     }
 
     pub fn draw() {
-        let msg_ele = DOCUMENT.get_element_by_id(self.msg_id).unwrap("「msg」エレメントがありません").dyn_into::<HtmlInputElement>().unwrap();
-        let wnd_ele = DOCUMENT.get_element_by_id(self.msg_id).unwrap("「wnd」エレメントがありません").dyn_into::<HtmlInputElement>().unwrap();
+        let msg_ele = DOCUMENT.get_element_by_id(self.msg_id).unwrap("「msg」エレメントがありません").dyn_into::<web_sys::HtmlInputElement>().unwrap();
+        let wnd_ele = DOCUMENT.get_element_by_id(self.msg_id).unwrap("「wnd」エレメントがありません").dyn_into::<web_sys::HtmlInputElement>().unwrap();
         match mode {
             WndMode::Home => {
                 msg_ele.text_content = "やりたいことをえらんでください";
